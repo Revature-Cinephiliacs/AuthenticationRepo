17b3371323cd17ba100546c18fbbe72c63d65e75
Noureldin Kamel <noureldinashraf6@gmail.com>
2021-04-20 21:57:51 +0300

    adds initial push of skeleton of authenticationapi

---
+++ AuthenticationAPI/Controllers/AuthenticationController.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Service.Authenticators;
+
+namespace AuthenticationAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class AuthenticationController : ControllerBase
+    {
+        private readonly Auth0Helper _helper;
+
+        public AuthenticationController(Auth0Helper _helper)
+        {
+            this._helper = _helper;
+        }
+
+        [HttpGet]
+        [Authorize]
+        public ActionResult Get()
+        {
+            return Ok();
+        }
+
+        [HttpGet("userdata")]
+        [Authorize]
+        public async Task<ActionResult<Dictionary<string, string>>> GetUserData()
+        {
+            // if we need to get user data
+            // pass to helper
+            var dictionary = await _helper.GetUserAuth0Dictionary(this.Request);
+            return dictionary;
+        }
+
+
+    }
+}
